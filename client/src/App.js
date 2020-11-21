dff6ef29f9e097b3c4b5ab9cd25c3826a172c372
chanyoung <kipa1003@naver.com>
2020-11-22 01:55:38 +0900

    redux, client routes basic setup;

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {Provider} from 'react-redux';
+import {ConnectedRouter} from 'connected-react-router';
+import store, {history} from './store';
+import MyRouter from './routes/Router';
+
+const App = () => {
+  return (
+    <Provider store={store}>
+      <ConnectedRouter history={history}>
+        <MyRouter/>
+      </ConnectedRouter>
+    </Provider>
+  );
+}
+
+export default App;
---
+++ client/src/routes/Router.js
@@ -0,0 +1,13 @@
+import React, { Fragment } from 'react';
+import Header from '../components/Header';
+
+const MyRouter = () => {
+    return (
+        <Fragment>
+            <Header/>
+            <h1>BODY</h1>
+        </Fragment>
+    )
+}
+
+export default MyRouter;
